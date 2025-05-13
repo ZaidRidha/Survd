204064459cf6386fed33d75d5f2576409bf68483
ZaidRidha <zaidridha@yahoo.com>
2025-05-13 23:03:30 +0100

    Metro Fix

--- metro.config.js
+++ metro.config.js
@@ -5,4 +5,6 @@ const defaultConfig = getDefaultConfig(__dirname, {
 });
 defaultConfig.resolver.assetExts.push('cjs');
 
+defaultConfig.resolver.unstable_enablePackageExports = false;
+
 module.exports = defaultConfig;
